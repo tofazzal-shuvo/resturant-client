6cdafec60723833151bfe6bfaeb286ae4a3fba98
shahan953 <misbauddin1994@gmail.com>
2020-12-22 03:11:44 +0600

    updated build

---
+++ build/precache-manifest.03702e28d92011135dbe9489cf3e8db2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d73d65130dc1ba94a669f7c2e4786ea0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "671801ba682d1d022308",
+    "url": "/static/css/2.69515dbd.chunk.css"
+  },
+  {
+    "revision": "e52da85ff6bb2066b0ac",
+    "url": "/static/css/main.6f94719f.chunk.css"
+  },
+  {
+    "revision": "671801ba682d1d022308",
+    "url": "/static/js/2.0a43ea67.chunk.js"
+  },
+  {
+    "revision": "43758f821a7d3b66fbc3ff5982b0c985",
+    "url": "/static/js/2.0a43ea67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e52da85ff6bb2066b0ac",
+    "url": "/static/js/main.d7cb2224.chunk.js"
+  },
+  {
+    "revision": "67406277ac2776ee3866",
+    "url": "/static/js/runtime-main.686bacc6.js"
+  }
+]);
--- server.js
+++ server.js
@@ -17,5 +17,5 @@ app.get("/*", function (req, res) {
 });
 
 // setup server
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3003;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
