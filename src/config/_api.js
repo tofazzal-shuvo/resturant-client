b571a6c9f3194688f28d8031ffe6127a5edcc319
shahan953 <misbauddin1994@gmail.com>
2021-01-21 13:51:41 +0600

    update stable version

--- src/config/_api.js
+++ src/config/_api.js
@@ -1,4 +1,4 @@
-export const GRAPHQL_URL = "http://3.137.193.103:3007/";
-// export const GRAPHQL_URL = "http://192.168.0.106:3007/";
+// export const GRAPHQL_URL = "http://3.137.193.103:3007/";
+export const GRAPHQL_URL = "http://192.168.0.106:3007/";
 
 // export const WEB_APP = process.env.REACT_WEB_APP || 'https://ez-pm.herokuapp.com/';
