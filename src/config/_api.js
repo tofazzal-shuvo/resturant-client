c39ebd7da8bcf4712435a93e053e882d5abae5e6
tofazzal-shuvo <tofazzalhaque63@gmail.com>
2021-06-28 14:11:26 +0600

    sdklf

--- src/config/_api.js
+++ src/config/_api.js
@@ -1,6 +1,6 @@
 // export const GRAPHQL_URL = "http://3.137.193.103:3007/";
 // export const GRAPHQL_URL = "http://192.168.0.106:3007/";
-export const GRAPHQL_URL = "http://yellowcarte.herokuapp.com";
+export const GRAPHQL_URL = "https://yellowcarte.herokuapp.com";
 // export const GRAPHQL_URL = `http://118.179.129.213:3007`;
 // export const GRAPHQL_URL_WS = `ws://118.179.129.213:3007`;
 export const REACT_APP_IMAGE_URL =
