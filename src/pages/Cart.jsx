495e0b5a5f8c90784426332d312791a98de24a62
tofazzal-shuvo <tofazzalhaque63@gmail.com>
2021-07-11 02:09:40 +0600

    banner logo responsivness fixed

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -38,8 +38,8 @@ const Cart = () => {
         },
       });
       if (CreateOrder.success) {
-        // dispatch(clearCard());
-        // dispatch(clearNote());
+        dispatch(clearCard());
+        dispatch(clearNote());
         notification.success({
           message: CreateOrder.message,
           placement: "bottomRight",
