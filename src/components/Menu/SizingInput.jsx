44b80516d0aabe5689d50decdc2051ad23765824
tofazzal-shuvo <tofazzalhaque63@gmail.com>
2021-07-17 02:02:09 +0600

    sizing fixed for empty items on sizing

--- src/components/Menu/SizingInput.jsx
+++ src/components/Menu/SizingInput.jsx
@@ -4,7 +4,7 @@ import SingleSizing from "./SingleSizing";
 const SizingInput = ({ selectSizing, setSizing, options, defaultColor }) => {
   const defaultSelected = [];
   const shouldRender = options.find((data) => !!data?.items?.length);
-  if (!!shouldRender) return null;
+  if (!shouldRender) return null;
   return (
     <>
       <h2 style={{ fontSize: "18px", marginTop: "8px", color: defaultColor }}>
