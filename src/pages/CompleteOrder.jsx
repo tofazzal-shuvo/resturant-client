51b137e2c02f40426283a2b0288ea2f0ff6634b4
tofazzal-shuvo <tofazzalhaque63@gmail.com>
2021-07-26 12:17:30 +0600

    call waiter added to complete order page

--- src/pages/CompleteOrder.jsx
+++ src/pages/CompleteOrder.jsx
@@ -1,17 +1,37 @@
-import { Button } from "antd";
+import { useMutation } from "@apollo/react-hooks";
+import { Button, Spin } from "antd";
 import React from "react";
 import { FormattedMessage } from "react-intl";
 import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { Banner } from "../components/Shared";
+import { CALL_WEITER } from "../graphql/modules";
+import { showNotification } from "../util";
 
 const CallWAITER = () => {
   const history = useHistory();
   const defaultColor = useSelector(
     (state) => state?.info?.resTemplate?.general?.defaultColor || ""
   );
+  const tableId = useSelector((state) => state?.info?.tableId || "");
 
-  const redirect = () => history.push("/ask-for-waiter");
+  const [onCallWeiter, { loading: callLoader }] = useMutation(CALL_WEITER);
+  const waiterCall = async () => {
+    try {
+      const {
+        data: { CallWaiter },
+      } = await onCallWeiter({
+        variables: {
+          tableId,
+        },
+      });
+      showNotification(CallWaiter);
+      if (CallWaiter.success) {
+        history.push("/ask-for-waiter");
+      }
+    } catch (error) {}
+  };
+
   const redirectToOrder = () => history.push("/menu");
 
   const buttonStyle = {
@@ -27,7 +47,7 @@ const CallWAITER = () => {
   };
 
   return (
-    <>
+    <Spin spinning={callLoader}>
       <Banner text={<FormattedMessage id="APP.BANNER.ORDER_PLACED" />} />
 
       <div className="text-center pl-2 pr-2">
@@ -46,12 +66,12 @@ const CallWAITER = () => {
           <Button style={buttonStyle} onClick={redirectToOrder}>
             <FormattedMessage id="APP.COMPLETE_ORDER.BUTTON" />
           </Button>
-          <Button style={buttonStyle} onClick={redirect}>
+          <Button style={buttonStyle} onClick={waiterCall}>
             <FormattedMessage id="APP.WAITER.CALL_WAITER" />
           </Button>
         </div>
       </div>
-    </>
+    </Spin>
   );
 };
 
